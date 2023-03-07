0eb6409af759eaccb299b16cad30f6884eedfbe0
Adonis <Adonis11300522@gmail.com>
2023-03-07 10:50:41 -0500

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT( {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "/node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "/node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
+});
